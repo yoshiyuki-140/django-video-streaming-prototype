08cff38dad1f00a221c89ed54758fff0ac0e1176
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-23 22:16:12 +0900

    add : DBを作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    video = models.FileField(upload_to="videos/", null=True, blank=True)
+    thumbnail = models.ImageField(upload_to="thumbnails/", null=True, blank=True)
