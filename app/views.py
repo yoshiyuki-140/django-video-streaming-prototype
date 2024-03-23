27656d790cee65f2bb256ac6616ea5f1c392f2b5
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2024-03-23 22:46:32 +0900

    update : app/views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django import forms
+from django.views.generic import FormView
+from app.models import Video
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+
+class UploadVideoForm(forms.Form):
+    file = forms.FileField()
